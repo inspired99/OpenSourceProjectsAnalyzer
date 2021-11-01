a9403b5addeb98f8b33018d6512832d28e4b782d
vsplekhanov <sclerothorax@gmail.com>
2021-11-01 19:13:44 +0300

    add simple commits frequency analysis

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from argparse import ArgumentParser
-import requests
+from CommitFrequency import commit_freq
 import re
 
 
-def run(matched, count):
-    request_url = f'https://api.github.com/repos/{matched.group(4)}/{matched.group(5)}' \
-                  f'/commits?per_page={count}'
-    response = requests.get(request_url).json()
-    if type(response) is type(dict()):
-        print('\n', response)
-    else:
-        for i in range(len(response)):
-            print('\n', i + 1, ':', f'\n"{response[i].get("commit").get("message")}"')
-        print(f'\nTotal {len(response)} commits\n')
 
 
 if __name__ == '__main__':
@@ -28,7 +18,11 @@ if __name__ == '__main__':
         matched = re.match('((https:\/\/)|(http:\/\/))?github\.com\/([A-Za-z0-9\S]+)'
                            '\/([A-Za-z0-9\S]+)', url)
         if matched:
-            run(matched, min(args.n_commits, 100))
+            username = matched.group(4)
+            repo = matched.group(5)
+            print()
+            print(commit_freq.run(username, repo))
+            print()
         else:
             print('Sorry, it seems like your url is not valid'
                   '\n The valid url should look like that: https://github.com/username/reponame')
