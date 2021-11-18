d2b589c0cec8ddcbd9d558e6cd243d20ea067ac9
vsplekhanov <sclerothorax@gmail.com>
2021-11-18 21:35:25 +0300

    Init commit

---
+++ src/main/java/ru/hse/fcs/OpenSourceProjectsAnalyzer/OpenSourceProjectsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.fcs.OpenSourceProjectsAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceProjectsAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSourceProjectsAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hse/fcs/OpenSourceProjectsAnalyzer/OpenSourceProjectsAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.fcs.OpenSourceProjectsAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSourceProjectsAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
