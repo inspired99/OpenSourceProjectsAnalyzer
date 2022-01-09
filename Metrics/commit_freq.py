48e5bf5c8da3b70dd54cc5fc2ab721f7c0626464
vsplekhanov <sclerothorax@gmail.com>
2022-01-09 08:14:12 +0700

    fix bug with last month

--- Metrics/commit_freq.py
+++ Metrics/commit_freq.py
@@ -3,8 +3,10 @@ from datetime import datetime
 from github import Github
 
 
-def prev_month(month):
-    return month - 1 if month != 1 else 12
+def prev_month(now):
+    if now.month != 1:
+        return now.replace(month=now.month - 1)
+    return now.replace(year=now.year - 1, month=12)
 
 
 def get_info(username, repo):
@@ -14,7 +16,7 @@ def get_info(username, repo):
     g = Github(access_token)
     repo = g.get_repo(f'{username}/{repo}')
     av_commits_count = int(repo.get_commits(since=one_year_ago).totalCount / 12)
-    last_month = repo.get_commits(since=now.replace(month=prev_month(now.month))).totalCount
+    last_month = repo.get_commits(since=prev_month(now)).totalCount
 
     return f'Average commits count per month in last year: {av_commits_count}\n' \
            f'Commits count in the last month: {last_month}'
